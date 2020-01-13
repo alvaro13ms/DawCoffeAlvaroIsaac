34b28cf3786ce11f459decef8c20ad55afa64dd0
Alvaro <alvaro13ms@gmail.com>
2020-01-13 18:53:39 +0100

    Creación de la clase UsuarioAdministrador con sus atributos y métodos getters.

---
+++ src/daw/coffee/UsuarioAdministrador.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daw.coffee;
+
+/**
+ *
+ * @author Álvaro
+ */
+public class UsuarioAdministrador {
+
+    private final int NOMBREUSUARIO = 0112;
+    private final int CONTRASEÑA = 35813;
+
+    public int getNOMBREUSUARIO() {
+        return NOMBREUSUARIO;
+    }
+
+    public int getCONTRASEÑA() {
+        return CONTRASEÑA;
+    }
+
+}
