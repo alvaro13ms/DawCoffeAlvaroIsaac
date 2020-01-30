20d416f255d0254d109800a72b2b32fe6a40c7cb
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-01-30 12:46:49 +0100

    Corrección de errores con el Final double, eliminando los SET y colocando los valores en un lugar correcto

--- src/daw/coffee/Menu.java
+++ src/daw/coffee/Menu.java
@@ -17,20 +17,20 @@ public class Menu {
     Scanner teclado = new Scanner(System.in);
     //Creación de los atributos de los productos de la cafetera
     //Precio de los productos de la cafetera
-    private double CAFESOLO = 0.80;
-    private double CAFESOLOLARGO = 0.90;
-    private double CAFELECHE = 1.10;
-    private double CORTADO = 1;
-    private double CHOCOLATE = 1.40;
-    private double LECHEFC = 0.50;
+    private final double CAFESOLO;
+    private final double CAFESOLOLARGO ;
+    private final double CAFELECHE ;
+    private final double CORTADO ;
+    private final double CHOCOLATE ;
+    private final double LECHEFC ;
     //Atributos adicionales
     double dinero,cafe;
     //Creación del constructor de los atributos
     public Menu(double CAFESOLO, double CAFESOLOLARGO, double CAFELECHE, double CORTADO, double CHOCOLATE, double LECHEFC) {
         this.CAFESOLO = CAFESOLO;
-        this.CAFESOLOLARGO = CAFESOLOLARGO;
+        this.CAFESOLOLARGO =CAFESOLOLARGO;
         this.CAFELECHE = CAFELECHE;
-        this.CORTADO = CORTADO;
+        this.CORTADO =CORTADO;
         this.CHOCOLATE = CHOCOLATE;
         this.LECHEFC = LECHEFC;
     }
@@ -91,6 +91,12 @@ public class Menu {
 
     //Creación del constructor predeterminado
     public Menu() {
+         this.CAFESOLO = 0.80;
+        this.CAFESOLOLARGO =0.90;
+        this.CAFELECHE = 1.10;
+        this.CORTADO = 1;
+        this.CHOCOLATE = 1.40;
+        this.LECHEFC = 0.50;
     }
 
     //GET
@@ -118,29 +124,6 @@ public class Menu {
         return LECHEFC;
     }
 
-    //SET
-    public void setCAFESOLO(double CAFESOLO) {
-        this.CAFESOLO = CAFESOLO;
-    }
-
-    public void setCAFESOLOLARGO(double CAFESOLOLARGO) {
-        this.CAFESOLOLARGO = CAFESOLOLARGO;
-    }
-
-    public void setCAFELECHE(double CAFELECHE) {
-        this.CAFELECHE = CAFELECHE;
-    }
-
-    public void setCORTADO(double CORTADO) {
-        this.CORTADO = CORTADO;
-    }
-
-    public void setCHOCOLATE(double CHOCOLATE) {
-        this.CHOCOLATE = CHOCOLATE;
-    }
-
-    public void setLECHEFC(double LECHEFC) {
-        this.LECHEFC = LECHEFC;
-    }
-
 }
+
+
